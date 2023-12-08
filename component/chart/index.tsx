0144340ca66ad107d64eb7c37080e593a3f601d9
WebWizrd8 <superultradev000@gmail.com>
2023-12-08 11:05:04 -0600

    configure token chart

--- component/chart/index.tsx
+++ component/chart/index.tsx
@@ -2,81 +2,93 @@
 
 import axios from "axios";
 import { ChainId, TokenAddress } from "../../types";
-import { getAxiosConfig } from "../../utils";
+import getBars, { getAxiosConfig } from "../../utils";
 import { BASE_URL } from "../../constants";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { ColorType, IChartApi, TimeChartOptions, createChart } from 'lightweight-charts';
 
 interface ChartComponentProps {
 
 }
 
 const ChartComponent: React.FC<ChartComponentProps> = () => {
-  const getBars = async (chainId: ChainId, tokenAddress: TokenAddress) => {
-    const currentDate = new Date();
+  const chartContainerRef = useRef<HTMLDivElement>(null);
+  let chart: IChartApi | null = null;
 
-    // Get the timestamp for today (midnight) in seconds
-    const todayTimestamp = Math.floor(
-      new Date(
-        currentDate.getFullYear(),
-        currentDate.getMonth(),
-        currentDate.getDate()
-      ).getTime() / 1000
-    );
-
-    // Get the timestamp for yesterday (midnight) in seconds
-    const yesterday = new Date();
-    yesterday.setDate(currentDate.getDate() - 1);
-    const yesterdayTimestamp = Math.floor(
-      new Date(
-        yesterday.getFullYear(),
-        yesterday.getMonth(),
-        yesterday.getDate()
-      ).getTime() / 1000
-    );
-    const query = `
-    {
-      getBars(
-        symbol: "${tokenAddress}:${chainId}"
-        from: ${yesterdayTimestamp}
-        to: ${todayTimestamp}
-        resolution: "1"
-        quoteToken: token1
-      ) {
-        o
-        h
-        l
-        c
-        v
-        volume
-        s
-      }
-    }
-    `
-    const config = {
-      url: BASE_URL,
-      method: "post",
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: process.env.NEXT_PUBLIC_DEFINED_API,
-      },
-      data: JSON.stringify({ query }),
-    }
-
-    try {
-      const result = await axios(config);
-      return result.data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      throw error;
-    }
-  }
-  // const result = getBars(ChainId.ETH, TokenAddress.ETH);
-  // console.log(result, 'chart result:');
   useEffect(() => {
-    const result = getBars(ChainId.ETH, TokenAddress.ETH);
-    console.log(result, 'result: ');
+    const fetchDataAndRenderChart = async () => {
+      try {
+        const result = await getBars(ChainId.ETH, TokenAddress.ETH);
+
+        const chartOptions: TimeChartOptions = {
+          layout: {
+            textColor: 'black',
+            background: { type: ColorType.Solid, color: 'white' },
+            fontSize: 12,
+            fontFamily: '',
+          },
+          timeScale: {
+            timeVisible: true, // Display time scale
+            secondsVisible: true, // Display seconds on the time scale
+            borderColor: 'grey', // Color of the time scale border
+            visible: true, // Show time scale
+            rightOffset: 0,
+            barSpacing: 0,
+            minBarSpacing: 0,
+            fixLeftEdge: false,
+            fixRightEdge: false,
+            lockVisibleTimeRangeOnResize: false,
+            rightBarStaysOnScroll: false,
+            borderVisible: false,
+            shiftVisibleRangeOnNewBar: false,
+            allowShiftVisibleRangeOnWhitespaceReplacement: false,
+            ticksVisible: false,
+            uniformDistribution: false,
+            minimumHeight: 0
+          },
+          localization: undefined,
+          width: 0,
+          height: 0,
+          autoSize: false,
+          watermark: undefined,
+          leftPriceScale: undefined,
+          rightPriceScale: undefined,
+          overlayPriceScales: undefined,
+          crosshair: undefined,
+          grid: undefined,
+          handleScroll: false,
+          handleScale: false,
+          kineticScroll: undefined,
+          trackingMode: undefined
+        };
+
+        const container = chartContainerRef.current;
+        if (container) {
+          chart = createChart(container, chartOptions);
+          // Add series and other chart configurations here
+          const baselineSeries = chart.addBaselineSeries({ baseValue: { type: 'price', price: 25 }, topLineColor: 'rgba( 38, 166, 154, 1)', topFillColor1: 'rgba( 38, 166, 154, 0.28)', topFillColor2: 'rgba( 38, 166, 154, 0.05)', bottomLineColor: 'rgba( 239, 83, 80, 1)', bottomFillColor1: 'rgba( 239, 83, 80, 0.05)', bottomFillColor2: 'rgba( 239, 83, 80, 0.28)' });
+          const { o: openPrices, t: timestamps } = result.getBars;
+          const transformedData = openPrices.map((value, index) => ({
+            value,
+            time: timestamps[index],
+          }));
+          baselineSeries.setData(transformedData);
+
+          chart.timeScale().fitContent();
+        } else {
+          console.error('Container element not found');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchDataAndRenderChart();
   }, [])
-  return <div>This is Chart</div>
+
+  return (
+    <div id="container" style={{ width: '100%', height: '800px' }} ref={chartContainerRef}></div>
+  )
 }
 
 export default ChartComponent;
--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { BASE_URL } from "../constants";
+import { ChainId, TokenAddress } from "../types";
 
 export const getAxiosConfig = () => {
   return {
@@ -9,3 +11,65 @@ export const getAxiosConfig = () => {
     },
   };
 }
+
+const getBars = async (chainId: ChainId, tokenAddress: TokenAddress) => {
+  const currentDate = new Date();
+
+  // Get the timestamp for today (midnight) in seconds
+  const todayTimestamp = Math.floor(
+    new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate()
+    ).getTime() / 1000
+  );
+
+  // Get the timestamp for yesterday (midnight) in seconds
+  const yesterday = new Date();
+  yesterday.setDate(currentDate.getDate() - 1);
+  const yesterdayTimestamp = Math.floor(
+    new Date(
+      yesterday.getFullYear(),
+      yesterday.getMonth(),
+      yesterday.getDate()
+    ).getTime() / 1000
+  );
+  const query = `
+  {
+    getBars(
+      symbol: "${tokenAddress}:${chainId}"
+      from: ${yesterdayTimestamp}
+      to: ${todayTimestamp}
+      resolution: "60"
+      quoteToken: token1
+    ) {
+      c
+      h
+      l
+      o
+      s
+      t
+      v
+    }
+  }
+  `
+  const config = {
+    url: BASE_URL,
+    method: "post",
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: process.env.NEXT_PUBLIC_DEFINED_API,
+    },
+    data: JSON.stringify({ query }),
+  }
+
+  try {
+    const result = await axios(config);
+    return result.data.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export default getBars;
